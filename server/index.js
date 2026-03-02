c5c06146890ff0cad349f06f17da940b5bf7e9d4
mrconfused22-dev <emailtoguddi@gmail.com>
2026-03-03 02:50:49 +0530

    main

--- server/index.js
+++ server/index.js
@@ -25,7 +25,10 @@ const io = new Server(server, {
 });;
 
 app.use(helmet({ contentSecurityPolicy: false }));
-app.use(cors());
+app.use(cors({
+  origin: ["https://ghost-chat-client.onrender.com", "http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json({ limit: "10kb" }));
 
 // Global limiter
