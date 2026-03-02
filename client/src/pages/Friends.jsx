fddd974d5e6c312df4f08cdf61aa0ed6062e7c0e
mrconfused22-dev <emailtoguddi@gmail.com>
2026-03-03 02:01:59 +0530

    Update Friends.jsx

--- client/src/pages/Friends.jsx
+++ client/src/pages/Friends.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const API = 'http://localhost:3001/api'
+const API = 'https://ghost-chat-server-muzw.onrender.com/api'
 
 function Friends({ user }) {
   const [tab, setTab] = useState('friends')
